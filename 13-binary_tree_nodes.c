9092038f1b334bcec9fc4d89044f6a3135edef57
Austin <Martinaustin319@gmail.com>
2023-11-30 03:52:35 -0800

    number of nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - count number if nodes
+ * @tree: pointer to binary tree
+ * Return: number of nodes if successful or 0 if not
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (tree)
+	{
+		count += (!tree->left || !tree->right) ? 1 : 0;
+		count += (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	}
+	return (count);
+}
