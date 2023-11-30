40fd82e6f235e184e177adb0fd6871d8b5ed345b
Austin <Martinaustin319@gmail.com>
2023-11-30 04:19:48 -0800

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,3 +12,23 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (binary_tree_height(tree->left) - binary_tree_height(tree->right);
 	return (0);
 }
+
+
+/**
+ * binary_tree_height - calculates the height of a binary tree
+ * @tree: pointer to binary tree
+ * Return: height of the bonary tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lef = 0, rig = 0;
+
+	if (tree)
+	{
+		lef = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		rig = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((lef > rig) ? lef : rig);
+	}
+	return (0);
+}
