2190e3463a672f1c0e6524c9984cf3a3c7e73887
Austin <Martinaustin319@gmail.com>
2023-11-30 00:47:56 -0800

    binary trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,9 +12,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *ptr = malloc(sizeof(binary_tree_t));
 
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
+
 	ptr->n = value;
 	ptr->parent = parent;
 	ptr->left = NULL;
--- binary_trees.h
+++ binary_trees.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
 
 /**
  *struct binary_tree_s - Binary tree node
@@ -22,3 +23,7 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif
