37094e862e7a868339d260f0716c2065b3e1cf8f
Austin <Martinaustin319@gmail.com>
2023-11-30 01:16:18 -0800

    node on the left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,14 +10,15 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
-	new = binary_tree_node(parent, value);
-	if (new == NULL)
-		return (NULL);
 
 	if (parent == NULL)
-	{
+		return (NULL);
+
+	new = binary_tree_node(parent, value);
+
+	if (new == NULL)
 		return (NULL)
-	}
+
 	if (parent->left != NULL)
 	{
 		new->left = parent->left;
