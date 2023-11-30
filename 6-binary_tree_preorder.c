e7875a804a90262c4fc29e617e818213bac9e0e3
Austin <Martinaustin319@gmail.com>
2023-11-30 02:21:35 -0800

    preorder

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - traverse a binary tree recursively
+ * @tree: pointer to the binart tree
+ * @func: pointer to a function
+ * Return: void
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder
+}
