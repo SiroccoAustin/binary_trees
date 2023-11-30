0cce19b9575717bf4ded075a99f9490b24f526e3
Austin <Martinaustin319@gmail.com>
2023-11-30 02:41:45 -0800

    postorder

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - tranverse a binary tree
+ * @tree: pointer to binary tree
+ * @func: pointer to a function
+ * Return: void
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
